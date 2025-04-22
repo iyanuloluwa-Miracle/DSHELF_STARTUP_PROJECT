a8549d435cf53438a9c894fb0b9744c55580c9aa
iyanuloluwa-Miracle <iyanudina@gmail.com>
2025-04-22 21:29:04 +0100

    fix: add 'https://dshelf.store/' to allowed CORS origins

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5173',
-  'https://dshelf-rust.vercel.app'
+  'https://dshelf-rust.vercel.app',
+  'https://dshelf.store/'
 ];
 
 // Configure CORS options
