743c336784dc25a405f929925f8763eb2208cc94
iyanuloluwa-Miracle <iyanudina@gmail.com>
2025-05-07 13:18:03 +0100

    feat: Db partition feature

--- config/env.js
+++ config/env.js
@@ -1,9 +1,11 @@
 // config/env.js
 require('dotenv').config();
 
+const isTest = process.env.NODE_ENV === 'test';
+
 module.exports = {
     PORT: process.env.PORT || 5000,
-    MONGODB_URI: process.env.MONGODB_URI,
+    MONGODB_URI: isTest ? process.env.MONGODB_URI_TEST : process.env.MONGODB_URI,
     JWT_SECRET: process.env.JWT_SECRET,
     EMAIL_HOST: process.env.EMAIL_HOST,
     EMAIL_PORT: process.env.EMAIL_PORT,
