eccd51fbc5ca225c0a9326e6a031123a6966af66
iyanuloluwa-Miracle <iyanudina@gmail.com>
2025-05-14 12:43:22 +0100

    fix: add MONGODB_URI to environment configuration

--- config/env.js
+++ config/env.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 module.exports = {
     PORT: process.env.PORT || 5000,
+    MONGODB_URI:process.env.MONGODB_URI,
     JWT_SECRET: process.env.JWT_SECRET,
     EMAIL_HOST: process.env.EMAIL_HOST,
     EMAIL_PORT: process.env.EMAIL_PORT,
