db25d2dcc2ec82b4101b8a6bfdafa1c1994d7d6b
iyanuloluwa-Miracle <iyanudina@gmail.com>
2025-05-14 12:37:56 +0100

    fix: remove MONGODB_URI handling for test environment

--- config/env.js
+++ config/env.js
@@ -1,11 +1,9 @@
 // config/env.js
 require('dotenv').config();
 
-const isTest = process.env.NODE_ENV === 'test';
 
 module.exports = {
     PORT: process.env.PORT || 5000,
-    MONGODB_URI: isTest ? process.env.MONGODB_URI_TEST : process.env.MONGODB_URI,
     JWT_SECRET: process.env.JWT_SECRET,
     EMAIL_HOST: process.env.EMAIL_HOST,
     EMAIL_PORT: process.env.EMAIL_PORT,
